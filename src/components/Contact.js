1d2d88407ed33ad704372c0ce02779f7325eddbe
OliveiraDCI <oliveira.sein.dci@gmail.com>
2022-10-21 20:13:06 +0200

    Exercise concluded.

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,7 +1,23 @@
-import React from "react";
+import { useState, useEffect } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 const Contact = () => {
-  return <h1>Contact</h1>;
+  const navigate = useNavigate();
+
+  const [state, setState] = useState("");
+
+  useEffect(() => {
+    if (state) navigate(state);
+  });
+
+  return (
+    <>
+      <h1>Contact</h1>
+      <Link to="/">
+        <button onClick={() => setState("/home")}>submit</button>
+      </Link>
+    </>
+  );
 };
 
 export default Contact;
